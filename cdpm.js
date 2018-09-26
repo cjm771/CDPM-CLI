9530ea949a250fae96d8d6b032e94bdba59bfdc0
Chris Malcolm <CMalcolm@nbbj.com>
2018-09-25 17:45:55 -0700

    [WIP] working on dyn py tools

--- cdpm.js
+++ cdpm.js
@@ -1,38 +1,76 @@
 #!/usr/bin/env node
-
+const dynReader = require('./lib/dynReader.js');
 const program = require('commander');
+const CMD_NAME = 'cdpm';
+
+const commands = {
+    dyn: {
+        description: 'Dynamo related commands',
+        subCommands: {
+            'py-ls' : {
+                args: false,
+                longName: 'list-python-nodes', 
+                description: 'Lists all the python nodes in file and their status',
+                routine: (file) => {
+                    dynReader.isDynFile(file).then( (isFile) => {
+                        console.log('congrats we doin this!');
+                    }).catch((error) => {
+                        console.log(file + ' is not a valid dynamo file! ' + error);
+                    });
+                }
+            }, 
+            'py-unpack' : {
+                args: '<file>',
+                longName: 'unpack-python-nodes', 
+                description: 'Unpacks all python nodes'
+            },
+            'py-pack' : {
+                args: '<file>',
+                longName: 'pack-python-nodes', 
+                description: 'Packs up all python nodes'
+            }
+        }
+    }
+}
 
 // start with version
 program
   .version('0.0.1')
   .description('Computational Design Package Management - CLI')
 
-// here are some commands
+
 program
-.command('helloWorld')
-.alias('hw')
-.description('echos hello world..')
-.action(() => {
-    console.log('hello world!')
+.command('dyn <dynCmd> <file>')
+.description(`Dynamo related routines.`)
+.action((dynCmd, file, flags) => {
+    if (commands.dyn.subCommands[dynCmd] && commands.dyn.subCommands[dynCmd].routine) {
+        commands.dyn.subCommands[dynCmd].routine(file, flags);
+    } else if (commands.dyn.subCommands[dynCmd]) {
+        console.log(`Unfortunately ${commands.dyn.subCommands[dynCmd].longName} isn't implemented yet!`);
+    }
 });
 
-// here's another
-program
-.command('hello <name>')
-.alias('h')
-.option('-!, --exclaim', 'exclaim it brah!')
-.option('-?, --question', 'question it brah?')
-.description('echos hello to persons name')
-.action((name, cmd) => {
-    let suffix = '';
-    if (cmd.exclaim) {
-        suffix += '!';
-    }
-    if (cmd.question) {
-        suffix += '?';
-    }
-    console.log(`Hello ${name}${suffix}`);
+//custom help
+program.on('--help', function(){
+    Object.keys(commands).forEach(cmdGroupName => {
+        const cmdGroupObj = commands[cmdGroupName];
+        console.log('');
+        console.log('In depth info');
+        console.log('========================');
+        
+        console.log('');
+        console.log(`${cmdGroupObj.description}`);
+        console.log(`-----------------------`);
+        console.log('');
+        Object.keys(cmdGroupObj.subCommands).forEach(subcommandName => {
+            const {args, longName, description} = cmdGroupObj.subCommands[subcommandName];
+            console.log(`${CMD_NAME} ${cmdGroupName} ${subcommandName} ${args || ''} \t${description}`);
+        });
+        console.log('');
+        console.log('');
+    });
 });
+
 
 
 program.parse(process.argv);
