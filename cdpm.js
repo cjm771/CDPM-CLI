a38a51290d9188beaf7b0ff2760f05731735f518
Chris Malcolm <CMalcolm@nbbj.com>
2018-09-26 15:49:16 -0700

    [WIP] dynamo traverser tools buit out..dyn-find-nodeId, dyn-find-nodeTypes

--- cdpm.js
+++ cdpm.js
@@ -3,20 +3,28 @@ const dynReader = require('./lib/dynReader.js');
 const program = require('commander');
 const CMD_NAME = 'cdpm';
 
+String.prototype.fixedLength = function (length=50) {
+let str =this;
+if (str.length > length) {
+    const ellipsis = '...';
+    str = str.slice(0, length - ellipsis.length) + ellipsis;
+}
+while (str.length < length ) {
+    str += ' ';
+}
+return str;
+}
+
 const commands = {
     dyn: {
         description: 'Dynamo related commands',
         subCommands: {
             'py-ls' : {
-                args: false,
+                args: '<file>',
                 longName: 'list-python-nodes', 
                 description: 'Lists all the python nodes in file and their status',
-                routine: (file) => {
-                    dynReader.isDynFile(file).then( (isFile) => {
-                        console.log('congrats we doin this!');
-                    }).catch((error) => {
-                        console.log(file + ' is not a valid dynamo file! ' + error);
-                    });
+                routine: function(file) {
+                  
                 }
             }, 
             'py-unpack' : {
@@ -28,6 +36,46 @@ const commands = {
                 args: '<file>',
                 longName: 'pack-python-nodes', 
                 description: 'Packs up all python nodes'
+            },
+            'find-nodeId': {
+                args: '<file> <nodeId>',
+                longName: 'find-node-by-id', 
+                description: 'Find node by node id. ex: `9d5edce5bbad41ff96a0dca1b9bdd8de`',
+                routine: function(file, nodeId, flags) {
+                    return dynReader.load(file)
+                    .then (() => {
+                        // grab nodes
+                        const node = dynReader.getNodebyID(nodeId);
+                        if (node === false) {
+                            throw 'Could not locate that id!';
+                        } else {
+                            dynReader.visualizeNode(node);
+                        }
+                    }).catch(error => {
+                        console.log('Error occurred when grabbing this id: ', error);
+                    })
+                }
+            },
+            'find-nodeTypes': {
+                args: '<file> <nodeType>',
+                longName: 'find-node-by-type', 
+                description: 'Find node by type. ex: `PythonScriptNode`',
+                routine: function(file, nodeType, flags) {
+                    return dynReader.load(file)
+                    .then (() => {
+                        // grab nodes
+                        dynReader.getNodesByType(nodeType).then(nodes => {
+                            nodes.forEach((node) => {
+                                dynReader.visualizeNode(node);
+                            })
+                        }).catch(error => {
+                            console.log('Error occurred when grabbing those nodes: ', error);
+                        })
+                    })
+                    .catch((error) => {
+                        console.log('Error! ' + error);
+                    });
+                }
             }
         }
     }
@@ -39,15 +87,29 @@ program
   .description('Computational Design Package Management - CLI')
 
 
-program
-.command('dyn <dynCmd> <file>')
-.description(`Dynamo related routines.`)
-.action((dynCmd, file, flags) => {
-    if (commands.dyn.subCommands[dynCmd] && commands.dyn.subCommands[dynCmd].routine) {
-        commands.dyn.subCommands[dynCmd].routine(file, flags);
-    } else if (commands.dyn.subCommands[dynCmd]) {
-        console.log(`Unfortunately ${commands.dyn.subCommands[dynCmd].longName} isn't implemented yet!`);
-    }
+// automatically create command names
+Object.keys(commands).forEach(cmdGroupName => {
+    const cmdGroupObj = commands[cmdGroupName];
+    Object.keys(cmdGroupObj.subCommands).forEach(subcommandName => {
+        const {args, longName, description, routine} = cmdGroupObj.subCommands[subcommandName];
+        program
+        .command(`${cmdGroupName}-${subcommandName} ${args}`)
+        .description(description)
+        .action(function() {
+            if (routine) {
+                routine.apply(null, [...arguments]);
+            } else {
+                console.log(`Unfortunately ${longName} isn't implemented yet!`);
+            }
+        });
+    });
+});
+
+
+// catch all
+program.command('*')
+.action(() => {
+    console.log('Operation not supported. Type `' + CMD_NAME + ' -h` for available options.');
 });
 
 //custom help
@@ -64,13 +126,17 @@ program.on('--help', function(){
         console.log('');
         Object.keys(cmdGroupObj.subCommands).forEach(subcommandName => {
             const {args, longName, description} = cmdGroupObj.subCommands[subcommandName];
-            console.log(`${CMD_NAME} ${cmdGroupName} ${subcommandName} ${args || ''} \t${description}`);
+            console.log(`${
+                (CMD_NAME + ' ' + 
+                cmdGroupName + '-' + 
+                subcommandName + ' ' + 
+                (args || ''))
+            .fixedLength(60)} ${description}`);
         });
         console.log('');
         console.log('');
     });
 });
-
 
 
 program.parse(process.argv);
